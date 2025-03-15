f67852a60459a9d93a2c5d9887fa4f9092ef0386
ren1kron <aleksy.pro35@gmail.com>
2025-03-15 23:31:45 +0300

    feat: implemented particles array in electrostatic page
    Поменял нынешнюю реализацию электростатика на реализацию с массивом из 3х частиц (в drawElectrostatic всё ещё отрисовывается одна точка, как и раньше). Добавил в форму электро ввод для всех 3х частиц.

--- src/components/electro/ManipulationElectrostaticForm.jsx
+++ src/components/electro/ManipulationElectrostaticForm.jsx
@@ -8,30 +8,25 @@ import {clearCanvas} from "../../util/addsToScene";
 const ManipulationElectrostaticForm = ({scene, camera, renderer}) => {
     const dispatch = useDispatch();
 
-    const [position, setPosition] = useState({position_x: 0, position_y: 0, position_z: 1, discharge: 1});
-    const [velocity, setVelocity] = useState({velocity_x: 1, velocity_y: 1, velocity_z: 1});
-    const [tension, setTension] = useState({tension_x: 1, tension_y: 1, tension_z: 1}); // напряжённость
-
+    const initialParticles = [
+        { position_x: 0, position_y: 0, position_z: 1, velocity_x: 1, velocity_y: 1, velocity_z: 1, discharge: 1 },
+        { position_x: 0, position_y: 0, position_z: 1, velocity_x: 1, velocity_y: 1, velocity_z: 1, discharge: 1 },
+        { position_x: 0, position_y: 0, position_z: 1, velocity_x: 1, velocity_y: 1, velocity_z: 1, discharge: 1 },
+    ];
+    const [particles, setParticles] = useState(initialParticles);
+    const [tension, setTension] = useState({ tension_x: 1, tension_y: 1, tension_z: 1 });
     const [gravity, setGravity] = useState(false);
     const [fallenTime, setFallenTime] = useState(2);
 
-    const updatePosition = (e) => {
-        const {name, value} = e.target;
-        setPosition(prevData => ({
-            ...prevData,
-            [name]: value,
-        }));
-
-    }
-
-    const updateVelocity = (e) => {
-        const {name, value} = e.target;
-        setVelocity(prevData => ({
-            ...prevData,
-            [name]: value,
-        }));
-
-    }
+    const updateParticleField = (index, field, value) => {
+        const updatedParticles = particles.map((particle, i) => {
+            if (i === index) {
+                return { ...particle, [field]: parseFloat(value) };
+            }
+            return particle;
+        });
+        setParticles(updatedParticles);
+    };
 
     const updateTension = (e) => {
         const {name, value} = e.target;
@@ -40,16 +35,15 @@ const ManipulationElectrostaticForm = ({scene, camera, renderer}) => {
             [name]: value,
         }));
 
-    }
+    };
 
     const handleOptionChange = (event) => {
-        if (event.target.value === "yes") setGravity(true);
-        else setGravity(false);
+        setGravity(event.target.value === "yes");
     };
 
     const updateTime = (e) => {
         setFallenTime(e.target.value);
-    }
+    };
 
     const updateCanvas = (e) => {
         e.preventDefault();
@@ -69,69 +63,144 @@ const ManipulationElectrostaticForm = ({scene, camera, renderer}) => {
         // Как я вижу вызов функции: DrawElectrostatic(particles, tension, gravity, fallenTime, scene, camera, renderer, dispatch);
         // particles = [{pos_x: ..., pos_y: ..., pos_z: ..., vel_x: ..., vel_y: ..., vel_z: ..., discharge: ...}, {...}, {...}], пока сделай максимум для передачи 3 частицы
         // пределы для слайдеров оставь те же
-        DrawElectrostatic(position, velocity, tension, gravity, fallenTime, scene, camera, renderer, dispatch);
-    }
+        // DrawElectrostatic(position, velocity, tension, gravity, fallenTime, scene, camera, renderer, dispatch);
+        DrawElectrostatic(particles, tension, gravity, fallenTime, scene, camera, renderer, dispatch);
+    };
 
     return (
-        <div className="lol">
-            <form onSubmit={e => e.preventDefault()}>
-                <div className="position">
-
-                    <h2>Начальные координаты и заряд частицы:</h2>
-                    <div className="sub-form-container">
-                        <div>
-                            <label htmlFor="position_x_slider">координата X:</label>
-                            <input type="range" id="position_x_slider" min="-1" max="1" name={"position_x"} step={0.1}
-                                   value={position.position_x} onChange={updatePosition}/>
-                            <span>{position.position_x}</span>
-                        </div>
-                        <div>
-                            <label htmlFor="position_y_slider">координата Y:</label>
-                            <input type="range" id="position_y_slider" min="-1" max="1" name={"position_y"} step={0.1}
-                                   value={position.position_y} onChange={updatePosition}/>
-                            <span>{position.position_y}</span>
-                        </div>
-                        <div>
-                            <label htmlFor="position_z_slider">координата Z:</label>
-                            <input type="range" id="position_z_slider" min="0.1" max="1" name={"position_z"} step={0.1}
-                                   value={position.position_z} onChange={updatePosition}/>
-                            <span>{position.position_z}</span>
-                        </div>
-                        <div>
-                            <label htmlFor="discharge_slider">значение заряда (*1e-9):</label>
-                            <input type="range" id="discharge_slider" min="-20" max="20" name={"discharge"} step={0.1}
-                                   value={position.discharge} onChange={updatePosition}/>
-                            <span>{position.discharge}</span>
-                        </div>
-                    </div>
-
-
-                </div>
-
-                <div className="velocity">
-                    <h2>Начальная скорость частицы:</h2>
-                    <div className="sub-form-container">
-                        <div>
-                            <label htmlFor="velocity_x_slider">по X:</label>
-                            <input type="range" id="velocity_x_slider" min="-5" max="5" name={"velocity_x"} step={0.1}
-                                   value={velocity.velocity_x} onChange={updateVelocity}/>
-                            <span>{velocity.velocity_x}</span>
-                        </div>
-                        <div>
-                            <label htmlFor="velocity_y_slider">по Y:</label>
-                            <input type="range" id="velocity_y_slider" min="-5" max="5" name={"velocity_y"} step={0.1}
-                                   value={velocity.velocity_y} onChange={updateVelocity}/>
-                            <span>{velocity.velocity_y}</span>
-                        </div>
-                        <div>
-                            <label htmlFor="velocity_z_slider">по Z:</label>
-                            <input type="range" id="velocity_z_slider" min="-5" max="5" name={"velocity_z"} step={0.1}
-                                   value={velocity.velocity_z} onChange={updateVelocity}/>
-                            <span>{velocity.velocity_z}</span>
-                        </div>
-                    </div>
-
-                </div>
+        <div className="manipulation-electrostatic-form">
+            <form onSubmit={(e) => e.preventDefault()}>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Параметр</th>
+                        <th>Частица 1</th>
+                        <th>Частица 2</th>
+                        <th>Частица 3</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Координата X</td>
+                        {particles.map((particle, index) => (
+                            <td key={`px-${index}`}>
+                                <input
+                                    type="range"
+                                    min="-1"
+                                    max="1"
+                                    step="0.1"
+                                    name="position_x"
+                                    value={particle.position_x}
+                                    onChange={(e) => updateParticleField(index, "position_x", e.target.value)}
+                                />
+                                <span>{particle.position_x}</span>
+                            </td>
+                        ))}
+                    </tr>
+                    <tr>
+                        <td>Координата Y</td>
+                        {particles.map((particle, index) => (
+                            <td key={`py-${index}`}>
+                                <input
+                                    type="range"
+                                    min="-1"
+                                    max="1"
+                                    step="0.1"
+                                    name="position_y"
+                                    value={particle.position_y}
+                                    onChange={(e) => updateParticleField(index, "position_y", e.target.value)}
+                                />
+                                <span>{particle.position_y}</span>
+                            </td>
+                        ))}
+                    </tr>
+                    <tr>
+                        <td>Координата Z</td>
+                        {particles.map((particle, index) => (
+                            <td key={`pz-${index}`}>
+                                <input
+                                    type="range"
+                                    min="0.1"
+                                    max="1"
+                                    step="0.1"
+                                    name="position_z"
+                                    value={particle.position_z}
+                                    onChange={(e) => updateParticleField(index, "position_z", e.target.value)}
+                                />
+                                <span>{particle.position_z}</span>
+                            </td>
+                        ))}
+                    </tr>
+                    <tr>
+                        <td>Заряд (*1e-9)</td>
+                        {particles.map((particle, index) => (
+                            <td key={`discharge-${index}`}>
+                                <input
+                                    type="range"
+                                    min="-20"
+                                    max="20"
+                                    step="0.1"
+                                    name="discharge"
+                                    value={particle.discharge}
+                                    onChange={(e) => updateParticleField(index, "discharge", e.target.value)}
+                                />
+                                <span>{particle.discharge}</span>
+                            </td>
+                        ))}
+                    </tr>
+                    <tr>
+                        <td>Скорость по X</td>
+                        {particles.map((particle, index) => (
+                            <td key={`vx-${index}`}>
+                                <input
+                                    type="range"
+                                    min="-5"
+                                    max="5"
+                                    step="0.1"
+                                    name="velocity_x"
+                                    value={particle.velocity_x}
+                                    onChange={(e) => updateParticleField(index, "velocity_x", e.target.value)}
+                                />
+                                <span>{particle.velocity_x}</span>
+                            </td>
+                        ))}
+                    </tr>
+                    <tr>
+                        <td>Скорость по Y</td>
+                        {particles.map((particle, index) => (
+                            <td key={`vy-${index}`}>
+                                <input
+                                    type="range"
+                                    min="-5"
+                                    max="5"
+                                    step="0.1"
+                                    name="velocity_y"
+                                    value={particle.velocity_y}
+                                    onChange={(e) => updateParticleField(index, "velocity_y", e.target.value)}
+                                />
+                                <span>{particle.velocity_y}</span>
+                            </td>
+                        ))}
+                    </tr>
+                    <tr>
+                        <td>Скорость по Z</td>
+                        {particles.map((particle, index) => (
+                            <td key={`vz-${index}`}>
+                                <input
+                                    type="range"
+                                    min="-5"
+                                    max="5"
+                                    step="0.1"
+                                    name="velocity_z"
+                                    value={particle.velocity_z}
+                                    onChange={(e) => updateParticleField(index, "velocity_z", e.target.value)}
+                                />
+                                <span>{particle.velocity_z}</span>
+                            </td>
+                        ))}
+                    </tr>
+                    </tbody>
+                </table>
 
                 <div className="tension">
                     <h2>Вектор напряжённости поля:</h2>
@@ -193,7 +262,7 @@ const ManipulationElectrostaticForm = ({scene, camera, renderer}) => {
                 </div>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default ManipulationElectrostaticForm;
